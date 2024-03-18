82e067cfeed70cd1be53883cbb11907dfa24faf1
Harish Sharma <harish.s@idsil.com>
2024-03-18 11:33:06 +0530

    for refreshing problem

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://boisterous-pie-24f5d7.netlify.app/',
+  base: 'https://boisterous-pie-24f5d7.netlify.app/',
 })
